202f7859d0869ce68fba898402d60bdff4fc9f25
Christopher Motl <cmotl@cmotl.com>
2012-01-27 14:42:12 -0600

    Initial Commit

---
+++ DraggableView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DraggableView
+//
+//  Created by Christopher Motl on 1/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DraggableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DraggableView
+//
+//  Created by Christopher Motl on 1/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
